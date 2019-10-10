a87860f5e60553f1c2749ad5501553e36d40f7a0
Ryota Sakurai <nkzsdy@gmail.com>
2019-10-10 22:07:14 +0900

    ユーザーのmodel specを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    context '正常系' do
+      context 'メールアドレスとパスワードがどちらも入力されているとき' do
+        it '登録できること' do
+          user = User.new(email: "hoge@fuga.com", password: "hogefuga")
+          expect(user).to be_valid
+        end
+      end
+    end
+
+    context '異常系' do
+      context 'メールアドレスだけが入力されていないとき' do
+        it '登録できないこと' do
+          user = User.new(password: "hogefuga")
+          expect(user).to be_invalid
+        end
+      end
+
+      context 'パスワードだけが入力されていないとき' do
+        it '登録できないこと' do
+          user = User.new(email: "hoge@fuga.com")
+          expect(user).to be_invalid
+        end
+      end
+
+      context 'メールアドレスとパスワードがどちらも入力されていないとき' do
+        it '登録できないこと' do
+          user = User.new
+          expect(user).to be_invalid
+        end
+      end
+    end
+  end
 end
